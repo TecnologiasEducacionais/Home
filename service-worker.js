504604ee9bec0e9ce551f319404ed6786e72fb23
Tecnologias Educacionais <te@unisuam.edu.br>
2024-09-03 12:11:57 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/styles.css',  // se houver um arquivo CSS separado
+                '/script.js',   // se houver um arquivo JS separado
+                '/icon-192x192.png',
+                '/icon-512x512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
